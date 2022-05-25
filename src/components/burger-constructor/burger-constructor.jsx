8d98f3adb797823389a00643ce435922eaa29876
Artem Benda <abenda@inbox.ru>
2022-05-25 10:15:44 +0300

    refactor - замена классовых компонентов функциональными
    refactor - замена классовых компонентов функциональными

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -4,55 +4,53 @@ import styles from './burger-constructor.module.css'
 import { ingredientPropType } from "../../utils/prop-types";
 import PropTypes from "prop-types";
 
-class BurgerConstructor extends React.Component {
-    render() {
-        const totalAmount = this.props.selectedIngredients.map(ingredient => ingredient.price).reduce((a, b) => a + b, 0) +
-            this.props.bunIngredient.price * 2
+function BurgerConstructor(props) {
+    const totalAmount = props.selectedIngredients.map(ingredient => ingredient.price).reduce((a, b) => a + b, 0) +
+        props.bunIngredient.price * 2
 
-        return (
-            <section className={styles.container}>
-                <article className={styles.fixedContent + ' pr-3 pl-15'}>
-                    <ConstructorElement
-                            type="top"
-                            isLocked={true}
-                            text={`${this.props.bunIngredient.name} (верх)`}
-                            price={this.props.bunIngredient.price}
-                            thumbnail={this.props.bunIngredient.image}
-                        />
-                </article>
-                <article className={styles.scrollableContent + ' custom-scroll'}>
-                    { this.props.selectedIngredients.map((ingredient, index) => (
-                        <div key={index} className={styles.ingredientItemContainer}>
-                            <DragIcon type="primary"/>
-                            <ConstructorElement
-                                text={ingredient.name}
-                                price={ingredient.price}
-                                thumbnail={ingredient.image}
-                            />
-                        </div>
-                    ))}
-                </article>
-                <article className={styles.fixedContent + ' pt-2 pr-3 pl-15'}>
-                    <ConstructorElement
-                        type="bottom"
+    return (
+        <section className={styles.container}>
+            <article className={styles.fixedContent + ' pr-3 pl-15'}>
+                <ConstructorElement
+                        type="top"
                         isLocked={true}
-                        text={`${this.props.bunIngredient.name} (низ)`}
-                        price={this.props.bunIngredient.price}
-                        thumbnail={this.props.bunIngredient.image}
+                        text={`${props.bunIngredient.name} (верх)`}
+                        price={props.bunIngredient.price}
+                        thumbnail={props.bunIngredient.image}
                     />
-                </article>
-                <article className={styles.fixedContent + ' pt-10 pb-10 pr-4 pl-4'}>
-                    <span className="text text_type_digits-medium pr-1">{totalAmount}</span>
-                    <CurrencyIcon type="primary" />
-                    <span className="ml-4">
-                        <Button type="primary" size="medium">
-                            Оформить заказ
-                        </Button>
-                    </span>
-                </article>
-            </section>
-        )
-    }
+            </article>
+            <article className={styles.scrollableContent + ' custom-scroll'}>
+                { props.selectedIngredients.map((ingredient, index) => (
+                    <div key={index} className={styles.ingredientItemContainer}>
+                        <DragIcon type="primary"/>
+                        <ConstructorElement
+                            text={ingredient.name}
+                            price={ingredient.price}
+                            thumbnail={ingredient.image}
+                        />
+                    </div>
+                ))}
+            </article>
+            <article className={styles.fixedContent + ' pt-2 pr-3 pl-15'}>
+                <ConstructorElement
+                    type="bottom"
+                    isLocked={true}
+                    text={`${props.bunIngredient.name} (низ)`}
+                    price={props.bunIngredient.price}
+                    thumbnail={props.bunIngredient.image}
+                />
+            </article>
+            <article className={styles.fixedContent + ' pt-10 pb-10 pr-4 pl-4'}>
+                <span className="text text_type_digits-medium pr-1">{totalAmount}</span>
+                <CurrencyIcon type="primary" />
+                <span className="ml-4">
+                    <Button type="primary" size="medium">
+                        Оформить заказ
+                    </Button>
+                </span>
+            </article>
+        </section>
+    );
 }
 
 BurgerConstructor.propTypes = {
--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -3,19 +3,17 @@ import React from "react";
 import styles from './burger-ingredient.module.css'
 import PropTypes from "prop-types";
 
-class BurgerIngredient extends React.Component {
-    render() {
-        return (
-            <article className={styles.container + ' pt-6 pb-10 pl-4 pr-4'}>
-                <div className={styles.imageContainer}>
-                    <img src={this.props.imageLarge} alt={this.props.name} className={styles.ingredientImage} />
-                    <Counter count={1} />
-                </div>
-                <p className={styles.priceText + ' pt-1'}><span className="text text_type_digits-default pr-1">{this.props.price}</span> <CurrencyIcon type="primary" /></p>
-                <p className={styles.nameText + ' pt-1'}><span className='text text_type_main-default'>{this.props.name}</span></p>
-            </article>
-        )
-    }
+function BurgerIngredient(props) {
+    return (
+        <article className={styles.container + ' pt-6 pb-10 pl-4 pr-4'}>
+            <div className={styles.imageContainer}>
+                <img src={props.imageLarge} alt={props.name} className={styles.ingredientImage} />
+                <Counter count={1} />
+            </div>
+            <p className={styles.priceText + ' pt-1'}><span className="text text_type_digits-default pr-1">{props.price}</span> <CurrencyIcon type="primary" /></p>
+            <p className={styles.nameText + ' pt-1'}><span className='text text_type_main-default'>{props.name}</span></p>
+        </article>
+    );
 }
 
 BurgerIngredient.propTypes = {
